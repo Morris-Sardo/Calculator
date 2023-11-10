4130bb868943fa61767c49965296c6b0c1f2701d
Morris sardo <zlac176@live.rhul.ac.uk>
2023-11-10 18:20:40 +0000

    i am try

---
+++ CW2/src/test/java/application/NumStackTest.java
@@ -0,0 +1,27 @@
+package application;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+/**
+ * This class has been create to test.
+ * @author papap
+ *
+ */
+class NumStackTest {
+  
+  private NumStack numStack;
+
+  //test 1 create class.
+  @Test
+  void createClasstest() {
+    NumStack numStack = new NumStack();
+    
+  }
+  
+  //test 2 
+  @Test
+  void () {
+    
+  }
+
+}
