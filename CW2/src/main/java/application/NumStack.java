478693906626e7296395b3ef1901e573fdfadf5a
Morris sardo <zlac176@live.rhul.ac.uk>
2023-11-11 14:40:04 +0000

    Added javadoc in NumStack

--- CW2/src/main/java/application/NumStack.java
+++ CW2/src/main/java/application/NumStack.java
@@ -1,7 +1,8 @@
 package application;
 
 /**
- * This class is...
+ * This class implement stack using only Entry number. The class in each method it call
+ * 
  * 
  * @author papap
  *
@@ -9,15 +10,18 @@ package application;
 
 public class NumStack {
 
-  private Stack numStack;
- 
+  private Stack numStack; // feild use to call all capability of the Stack class.
 
+
+  /**
+   * Yhis constructor use to initialize the object stack.
+   */
   public NumStack() {
     this.numStack = new Stack();
   }
 
   /**
-   * This metod is responsible to add and entry float into stack.
+   * This metod is responsible to add and entry float into stack, calling pop method of Stack class.
    * 
    * @param number is a paramenter to add all the number into calulator.
    */
@@ -35,16 +39,26 @@ public class NumStack {
    * @throws EmptyStack is exception throw everytime you try retreave a number with empty stack.
    */
   public Entry top() throws EmptyStack {
-    
+
     return numStack.top();
   }
-  
-  
-  public Integer size() {
-   
+
+  /**
+   * This method keep track of of the dimention of the stack.
+   * 
+   * @return number of the element into the stack.
+   */
+  public int size() {
+
     return numStack.size();
   }
 
+  /**
+   * This method retun the element on the top of stack calling the pop method int.
+   * 
+   * @return obtain and return element on the stack.
+   * @throws EmptyStack throw exception if element is in the first r second porsiozion
+   */
   public Entry pop() throws EmptyStack {
     return numStack.pop();
   }
--- CW2/src/test/java/application/NumStackTest.java
+++ CW2/src/test/java/application/NumStackTest.java
@@ -11,8 +11,8 @@ import org.junit.jupiter.api.Test;
 /**
  * The meaning of this class is create methods in numStack class and all the methodos in the
  * NumStack class all its method will work using the the mothod of the Stack class. allexception
- * implemented into Stack class. In other world the the meaning is to implement numStack class as it is
- * a Stack class wiith the only capability to get only float number entry.
+ * implemented into Stack class. In other world the the meaning is to implement numStack class as it
+ * is a Stack class wiith the only capability to get only float number entry.
  * 
  * @author papap
  *
@@ -41,7 +41,7 @@ class NumStackTest {
   // into pushTest() and puch(). Create entry object that get a float attribute.
   // Create object stack such that the method push in NumStak class will call push method
   // into Stack class.
-  //I had to add BadType exception as I am using Entry class.
+  // I had to add BadType exception as I am using Entry class.
   //
   @Test
   void Pushtest() throws EmptyStack, BadType {
@@ -52,7 +52,7 @@ class NumStackTest {
     assertEquals(randFloat, entry.getFloat());
 
   }
-  
+
   // test 3 check it if the exception throw the right text exception.
   @Test
   void topThrowsEmptyExceptionWhenEmpty() {
@@ -61,22 +61,30 @@ class NumStackTest {
     assertEquals("Stack is Empty", e.getMessage());
 
   }
-  
+
   // test 5 is create pop method in NumStack class.
   // I started to create size method such that every time push some thing into the stack the size
   // will increase.
-  // Next, I have created  pop method. To do so i had to add also all the exception(confirming the also 
-  //pop method in numStack retreave the functionality of the pop method in Stack class.
-  @Test 
-  void Poptest() throws EmptyStack {
-    
+  // Next, I have created pop method. To do so i had to add also all the exception(confirming the
+  // also
+  // pop method in numStack retreave the functionality of the pop method in Stack class.
+  // Finally i ad to add Badtype exception as popEntryt i have use getFloat() that has BadTyoe as
+  // exception.
+  @Test
+  void Poptest() throws EmptyStack, BadType {
+
     float randFloat = testValue.nextFloat() * (float) 100.0;
     numStack.push(randFloat);
-    assertEquals(1 , numStack.size());
-    numStack.push(randFloat);
-    assertNotEquals(1 , numStack.size());
-    
+    assertEquals(1, numStack.size());
+
+    float randFloat1 = testValue.nextFloat() * (float) 100.0;
+    numStack.push(randFloat1);
+    assertNotEquals(1, numStack.size());
+
     Entry popEntry = numStack.pop();
+    assertEquals(randFloat1, popEntry.getFloat());
+
+
   }
 
 }
