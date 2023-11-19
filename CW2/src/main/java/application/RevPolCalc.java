a14e1fcab085d23d3a3d5b1a97e537b3e07d6436
Morris sardo <zlac176@live.rhul.ac.uk>
2023-11-19 09:22:36 +0000

    test 3 failed.

--- CW2/src/main/java/application/RevPolCalc.java
+++ CW2/src/main/java/application/RevPolCalc.java
@@ -7,5 +7,21 @@ package application;
  *
  */
 public class RevPolCalc {
+  
+  private float result;
+  
+  public RevPolCalc(String expression) {
+    this.result = evaluate(expression);
+    
+  }
+  
+  private float evaluate(String expression) {
+    
+    return 7.0f;
+  }
+
+  public float getResult() {
+    return result;
+  }
 
 }
--- CW2/src/test/java/application/RevPolCalcTest.java
+++ CW2/src/test/java/application/RevPolCalcTest.java
@@ -1,6 +1,7 @@
 package application;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import org.junit.jupiter.api.Test;
 /**
  * This class ...
@@ -8,22 +9,31 @@ import org.junit.jupiter.api.Test;
  *
  */
 class RevPolCalcTest {
-
   
+  private RevPolCalc polCal; // ust to store the expression.
+
+ 
   //test 1 if class exist.
   @Test
   void RevPolClacClasstest() {
     
-    RevPolCalc polCal = new RevPolCalc();
+    polCal = new RevPolCalc("12+");
     
   }
   
-  //test 2 if constructor evaluate espression and store result.s
+  //test 2 if constructor evaluate espression and store result.
+  //Create constructor and getResult() method.
+  //Modified test 1( added agrguement).
+  //Test 3 add retrun different value.
   @Test
   void constructorEvaluateExpressiontest() {
     
-    RevPolCalc polCal = new RevPolCalc("34+");
+    polCal = new RevPolCalc("34+");
     assertEquals(7.0,polCal.getResult());
+    assertNotEquals(7.2, polCal.getResult());
+    
+    polCal = new RevPolCalc("512+4*+3-");
+    assertEquals(14.0,polCal.getResult());
     
   }
 
