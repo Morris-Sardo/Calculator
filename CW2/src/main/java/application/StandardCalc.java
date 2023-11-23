5d1bcbb748fa669fe13398ec2ca8d247fa6cf1f7
Morris sardo <zlac176@live.rhul.ac.uk>
2023-11-23 17:44:08 +0000

    test 2 passed.
    Implemented StandardCalc class. The class hass been tested before like it would return
    string but after the class has been improved to return the float point.

--- CW2/src/main/java/application/StandardCalc.java
+++ CW2/src/main/java/application/StandardCalc.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 /**
  * This class implement the calculation in form by using Stadard from.
  * 
@@ -7,30 +12,72 @@ package application;
  *
  */
 public class StandardCalc {
-
-  RevPolCalc rpCalc;
-
+  
+  private RevPolCalc rpCalc;
+  
   public StandardCalc() {
-
+    
     this.rpCalc = new RevPolCalc();
   }
+  
+  
+  //THIS IS NOT THE FINAL VERSION OF THE CLASS.
+  public float evaluate(String expr) throws InvalidExpression {
+    
+      //keep track od priority between all operator.
+      Map<Character, Integer> precedence = new HashMap<>();
+      precedence.put('+', 1);
+      precedence.put('-', 1);
+      precedence.put('*', 2);
+      precedence.put('/', 2);
+      precedence.put('(', -1); 
+      precedence.put(')', -1);
 
-  /**
-   * 
-   * @param expression
-   * @return
-   * @throws InvalidExpression
-   */
-  public float evaluate(String expression) throws InvalidExpression {
+      StringBuilder output = new StringBuilder(); //it used the final Reverved expression.
+      Deque<Character> stack = new LinkedList<>(); //it use to to handle the operator.
 
-    String[] token = expression.split(" ");
+      String[] tokens = expr.split("\\s+");
+      
+      //loop use to convert the espression form normal to reverse one.
+      //the conversion is been handle by hashmaps that in base of the prioriy assigned to the 
+      //operators it decide in collaboration with queue when adding into StringBuilder.
+      //The paranthesis won't be added in thr BuilderString.
+      //Is been throw excetpion if the the parentesis are not matched. 
+      //THIS 
+      for (String token : tokens) {
+          char c = token.charAt(0);
 
+          if (Character.isDigit(c)) {
+              output.append(token).append(' ');
+          } else if (c == '(') {
+              stack.push(c);
+          } else if (c == ')') {
+              while (!stack.isEmpty() && stack.peek() != '(') {
+                  output.append(stack.pop()).append(' ');
+              }
+              if (!stack.isEmpty() && stack.peek() == '(') {
+                  stack.pop();
+              } else {
+                  throw new InvalidExpression("Mismatched parentheses");
+              }
+          } else {
+              while (!stack.isEmpty() && precedence.getOrDefault(stack.peek(), -1) >= precedence.getOrDefault(c, -1)) {
+                  output.append(stack.pop()).append(' ');
+              }
+              stack.push(c);
+          }
+      }
 
-    return rpCalc.evaluate(token[0] + " " + token[2] + " " + token[1]);
+      while (!stack.isEmpty()) {
+          if (stack.peek() == '(') {
+              throw new InvalidExpression("Mismatched parentheses");
+          }
+          output.append(stack.pop()).append(' ');
+      }
+
+      return rpCalc.evaluate(output.toString().trim());
   }
-
-
-
 }
+
 
 
--- CW2/src/test/java/application/StandardCalcTest.java
+++ CW2/src/test/java/application/StandardCalcTest.java
@@ -1,41 +1,61 @@
 package application;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class StandardCalcTest {
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * The meaning of this class is testing all possible operations allowed by the calculator and to
+ * test the various possible faults.
+ * 
+ * @author papap
+ *
+ */
+class SandardCalcTest {
   
-  private StandardCalc calc;
   
-  
-  @BeforeEach
-  void setUp() {
-    
-    calc = new StandardCalc();
-    
-    
-  }
+  StandardCalc calc = new StandardCalc();
   
   //test 1
-  //Test addiction.
-  //create evaluate Method thre return 5.0.
+  //I have create class StandardCalss class.
+  //Create created hashmap such that it will keep track of priority of operator.
+  //use a BuilderString to save the expression converted in Reverse polish on.
+  //I used queue such that the operand i could push and pop of both side(this is good 
+  //because if a operand was behind to another one it could puch it into from the another side of it).
+  //Inside the loop variuos if and else statement with sopme of them with while loop. 
+  //In addition the loop check if elelent is operando or operator. If it s a operator check in hashmap
+  //which prioprit has(Higher number hegher priority) and add them in the back of number into BuildString.
+  //If it s parantesis check grup all numbers and operator into it.
+  //Is is been throw an exception if a parentesist is mismatched.
   @Test
-  void AdditionTest() throws InvalidExpression {
+  void AdditionTest() throws InvalidExpression{
     
-    
-    assertEquals(5, calc.evaluate("2 + 3"));
+    assertEquals(61f, calc.evaluate("2 * 32 + 3 - 6"));
   }
   
   //test 2
-  //evalute long enxpression.
+  //evaluate long esxpression with addition of parentheses. 
+  
   @Test
-  void LongAdditionTest() throws InvalidExpression {
+  void LongAdditionTest() throws InvalidExpression{
     
-    assertEquals(20,calc.evaluate("2 + 3 + 4 + 5 + 6"));
+    assertEquals( 10f, calc.evaluate("( 2 * 3 ) + 4"));
   }
   
   
-
-
+  //test 3 
+  //test if epression implemented by StandardCalc is compatible 
+  //with RevPolCalss class.
+  //Modified evaluate method form string to float.
+  //create contrutor use to inizialise RevPolCalc's object.
+  //Test 1 and test 2 now are failed as evaluate type is not String anymore.
+  
+//  @Test
+//  void StandCalcCompatibleWithRevPolCalctest() throws InvalidExpression {
+//    
+//    assertEquals(5f,calc.evaluate("3 + 2"));
+//  }
+  
 }
+
