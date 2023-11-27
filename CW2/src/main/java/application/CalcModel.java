b9d88984b46cee6123309999a0f992de8cb0cad4
Morris sardo <zlac176@live.rhul.ac.uk>
2023-11-27 18:20:05 +0000

    test 1 and test 2 in CalcModelTest passed.
    modified evaluate in CalcModel class  such that if infx is true evalute expression in standard form, false in reverse one.

--- CW2/src/main/java/application/CalcModel.java
+++ CW2/src/main/java/application/CalcModel.java
@@ -4,28 +4,34 @@ package application;
  * Evaluates an expression - the evaluation can be Standard (infix) or reverse polish.
  */
 public class CalcModel {
-  
-  //private Calculator revPol; //use to implement reversePol
-  private Calculator standard; // use to implemtent Standard calculation.
-  
-  
- /**
-  * Constructor..
-  */
-  public CalcModel() {
-    //this.revPol = new RevPolCalc();
-    this.standard = new StandardCalc();
-    
-  }
+
+  private Calculator revPol; // use to implement reversePol
+  private Calculator standCalc; // use to implemtent Standard calculation.
+
+
   /**
+   * Constructor..
+   */
+  public CalcModel() {
+    this.revPol = new RevPolCalc();
+    this.standCalc = new StandardCalc();
+
+  }
+
+  /**
+   * This method evaluate a Standard form or Reverse Polish form.
    * 
-   * @param expression
-   * @param infix
-   * @return 
-   * @throws InvalidExpression
+   * @param expression use as user input.
+   * @param infix used to decise whitch kind espreassion evaluate.
+   * @return reult of calcualtion.
+   * @throws InvalidExpression if the expression is not right format.
    */
   public float evaluate(String expression, Boolean infix) throws InvalidExpression {
-    
-    return standard.evaluate(expression);
+
+    if (infix) {
+      return standCalc.evaluate(expression);
+    } else {
+      return revPol.evaluate(expression);
+    }
   }
 }
--- CW2/src/test/java/application/CalcModelTest.java
+++ CW2/src/test/java/application/CalcModelTest.java
@@ -19,22 +19,47 @@ class CalcModelTest {
  }
   //test 1. This test implement standards calculation.
  //create field in Calculator, create Constructor and fixed return value to 5.
+ //Improving test randoming the numbers.
   @Test
-  void evaluateRevPolitest() throws InvalidExpression {
-   
-    String expression = "3 + 2"; 
-    assertEquals(5f,calcMod.evaluate(expression,true));
+  void evaluateStandCaldtest() throws InvalidExpression {
     
+    String addExpre = "3 + 2"; 
+    assertEquals(5f,calcMod.evaluate(addExpre,true));
+    
+    String subExpre = "6.3 - 2";
+    assertEquals(4.3f,calcMod.evaluate(subExpre, true));
+    
+    String timeExpre = "6.3 * 2";
+    assertEquals(12.6f,calcMod.evaluate(timeExpre, true));
+    
+    String divExpre = "6 / 2.8";
+    assertEquals(2.142857f,calcMod.evaluate(divExpre, true));
+      
  
   }
   
   //test 2. This test implement Rev polish calculation. 
   //
+  //
   @Test 
-  void evaluateStandardCalctest() throws InvalidExpression {
+  void evaluateRevPolCalctest() throws InvalidExpression {
     
-    String expression = "6 2 -";
-    assertEquals(4f,calcMod.evaluate(expression, false));
+    String subExpre = "6 2 -";
+    assertEquals(4f,calcMod.evaluate(subExpre, false));
     
+    String divExpre = "6.3 2 +";
+    assertEquals(8.3f,calcMod.evaluate(divExpre, false));
+    
+    String devExpre = "6 2.8 /";
+    assertEquals(2.142857f,calcMod.evaluate(devExpre, false));
+    
+    String timeExpre = "2 11 *";
+    assertEquals(22f,calcMod.evaluate(timeExpre, false));
+    
+    
+        
   }
+   
+  
+  
 }
