499a1ae05399238028559fb1b0de2fc65fd02869
Morris sardo <zlac176@live.rhul.ac.uk>
2023-12-02 12:04:58 +0000

    test 1 falied.
    Create MockView clasc.

--- CW2/src/test/java/application/CalcControllerTest.java
+++ CW2/src/test/java/application/CalcControllerTest.java
@@ -9,13 +9,16 @@ class CalcControllerTest {
 //  private CalcController calcContr;
 //  private CalcModel model;
 //  private CalcView view;
-  
-
+//  private boolean isInFix;
+//
 //  @Test
 //  void test() {
 //    
+////    String equation = "3 + 3";
+////    model = new CalcModel();
+////    calcContr = new CalcController(model.evaluate(equation,true),);
 //    
-//    fail("Not yet implemented");
 //  }
 
 }
+
---
+++ CW2/src/test/java/application/MockViewTest.java
@@ -0,0 +1,24 @@
+package application;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+/**
+ * 
+ * @author papap
+ *
+ */
+class MockViewTest {
+  private MockView mockView;
+  
+//  @BeforeEach
+//  void setUp() {
+//    mockView = new MockView();
+//  }
+
+  @Test
+  void test() {
+    mockView = new mockView();
+  }
+
+}
